4db6b4a5703b895f487e624227df036c8208a4e5
Yun <yun@pikicast.com>
2016-11-11 12:29:44 +0900

    @Captor 예제 추가.

--- src/main/java/day01/PikiPresenter.java
+++ src/main/java/day01/PikiPresenter.java
@@ -10,4 +10,9 @@ public class PikiPresenter implements Presenter {
         // networking...
         return "{result: production}";
     }
+
+    @Override
+    public void login(String id) {
+
+    }
 }
--- src/test/java/day01/PikiViewTest.java
+++ src/test/java/day01/PikiViewTest.java
@@ -3,6 +3,8 @@ package day01;
 import org.junit.Before;
 import org.junit.Rule;
 import org.junit.Test;
+import org.mockito.ArgumentCaptor;
+import org.mockito.Captor;
 import org.mockito.Mock;
 import org.mockito.junit.MockitoJUnit;
 import org.mockito.junit.MockitoRule;
@@ -22,23 +24,34 @@ public class PikiViewTest {
     @Mock
     Presenter presenter;
 
+    @Captor
+    ArgumentCaptor<String> argumentCaptor;
+
     String testResult = "{result: test}";
     PikiView pikiView;
 
     @Before
     public void setUp() {
         pikiView = new PikiView();
-
+        pikiView.setPresenter(presenter);
         when(presenter.getData()).thenReturn(testResult);
     }
 
     @Test
     public void verifyData() {
-        pikiView.setPresenter(presenter);
         pikiView.requestData();
 
         assertThat(pikiView.getData(), is(testResult));
 
         verify(presenter, times(1)).getData();
+    }
+
+    @Test
+    public void login() {
+        pikiView.login();
+
+        verify(presenter).login(argumentCaptor.capture());
+
+        assertThat("some id", is(argumentCaptor.getValue()));
     }
 }
