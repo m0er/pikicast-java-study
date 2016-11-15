4707a680337ed64c417ccb50cfe00897e62d02f1
ChoiAiden <AidenChoi@ChoiAidenui-MacBook-Pro-2.local>
2016-11-15 22:05:08 +0900

    Student 클래스 생성 및 테스트 추가.

---
+++ src/main/java/day03/sis/Student.java
@@ -0,0 +1,10 @@
+package day03.sis;
+
+/**
+ * Created by AidenChoi on 2016. 11. 15..
+ */
+public class Student {
+    public Student(String name) {
+
+    }
+}
---
+++ src/test/java/day03/sis/StudentTest.java
@@ -0,0 +1,14 @@
+package day03.sis;
+
+import org.junit.Test;
+
+/**
+ * Created by AidenChoi on 2016. 11. 15..
+ */
+public class StudentTest {
+
+    @Test
+    public void create() {
+        new Student("Jane Doe");
+    }
+}
