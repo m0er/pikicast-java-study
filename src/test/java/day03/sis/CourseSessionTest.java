7242c9c2655e1c949a2f5765be292bd1727d94aa
ChoiAiden <AidenChoi@ChoiAidenui-MacBook-Pro-2.local>
2016-11-15 22:43:56 +0900

    리팩토링.

--- src/test/java/day03/sis/CourseSessionTest.java
+++ src/test/java/day03/sis/CourseSessionTest.java
@@ -1,5 +1,6 @@
 package day03.sis;
 
+import org.junit.Before;
 import org.junit.Test;
 
 import static org.hamcrest.MatcherAssert.*;
@@ -10,14 +11,22 @@ import static org.mockito.Mockito.*;
  * Created by AidenChoi on 2016. 11. 15..
  */
 public class CourseSessionTest {
+    CourseSession courseSession;
+
+    @Before
+    public void setUp() {
+        courseSession = new CourseSession("ENGL", "101");
+    }
 
     @Test
     public void create() {
-        CourseSession courseSession = new CourseSession("ENGL", "101");
         assertThat(courseSession.getDepartment(), is("ENGL"));
         assertThat(courseSession.getNumber(), is("101"));
         assertThat(courseSession.getNumberOfStudents(), is(0));
+    }
 
+    @Test
+    public void enroll() {
         Student student1 = new Student("Cain Divoe");
         courseSession.enroll(student1);
         assertThat(courseSession.getNumberOfStudents(), is(1));
