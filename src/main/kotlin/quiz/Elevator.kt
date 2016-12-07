990b8602e9c365850d3ce183aad2c0a68b61324b
Yun <yun@pikicast.com>
2016-12-07 17:04:17 +0900

    엘리베이터 퀴즈 와꾸.

---
+++ src/main/kotlin/quiz/Elevator.kt
@@ -0,0 +1,6 @@
+package quiz
+
+/**
+ * Created by AidenChoi on 2016. 12. 7..
+ */
+class Elevator
---
+++ src/test/kotlin/quiz/ElevatorTest.kt
@@ -0,0 +1,16 @@
+package quiz
+
+import org.junit.Test
+
+/**
+ * Created by AidenChoi on 2016. 12. 7..
+ */
+class ElevatorTest {
+    lateinit var elevator: Elevator
+
+    @Test
+    fun create() {
+        elevator = Elevator()
+    }
+
+}
